af0731e473fca735f02615e14673885360397c7a
ashutosh44-star <ashutoshchauhan149@gmail.com>
2025-02-08 15:58:40 +0530

    JWT Authentication

--- src/main/java/com/smartcontactmanager/EmailService/EmailService.java
+++ src/main/java/com/smartcontactmanager/EmailService/EmailService.java
@@ -33,7 +33,7 @@ public boolean sendEmail(String to,String message,String subject) {
  
 	boolean sent=false;
 //	String to = "ashutosh.jmd31@gmail.com";
-	String from="ashutoshchauhan149@gmail.com";
+	String from="xxxxxxxxx"; //----Used Dummy for git and now it will be skipped from next push onwards
 //	String subject="Mail sent via Smart Contact Management";
 //	String message="Message Received Successfully..!";
 	try {
