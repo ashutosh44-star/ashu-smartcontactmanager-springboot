0a1fadd36fb32891e203a3fe01025c71855f5304
ashutosh44-star <ashutoshchauhan149@gmail.com>
2025-02-09 17:50:59 +0530

    DB Configuration

--- src/main/java/com/smartcontactmanager/jwt/JwtResponse.java
+++ src/main/java/com/smartcontactmanager/jwt/JwtResponse.java
@@ -9,12 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
+
 public class JwtResponse {
 	
 	public JwtResponse(String token, String username) {
